884570da4f2e4de5cc07a3f96002ea08fd12082b
zhaopin123 <45994629+zhaopin123@users.noreply.github.com>
2019-05-05 20:54:48 +0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 app.register_blueprint(blueprint=blue, url_prefix='/app')
 
 # 配置数据库
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123456@119.23.76.19:3306/ihome'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123456@127.0.0.1:3306/ihome'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
